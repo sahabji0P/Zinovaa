287955becde123ee8a89392514c29d6acee333f9
Shashwat Jain <kcapshashwat@gmail.com>
2024-11-29 13:40:51 +0530

    Changes in header file

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -35,14 +35,14 @@ export default function Header() {
       <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50">
         <Banner showBanner={showBanner} isMobile={isMobile} />
         
-        <Link className='cursor-wait' href="/">
+        <Link className='cursor-' href="/">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center cursor-wait">
             <motion.h1 
               initial={{ opacity: 0, x: -20 }}
               animate={{ opacity: 1, x: 0 }}
               className={`${playfair.className} text-2xl sm:text-3xl md:text-4xl font-bold cursor-wait`}
             >
-              <span className="bg-gradient-to-r from-red-600 to-red-500 bg-clip-text text-transparent">
+              <span className="bg-gradient-to-r from-red-600 to-red-900 bg-clip-text text-transparent">
                 Zinovaa
               </span>
             </motion.h1>
