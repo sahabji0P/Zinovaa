bf7b1ea85e1cb2ba659e04bea5d65dc8c91ce30f
Shashwat Jain <kcapshashwat@gmail.com>
2025-02-07 03:26:44 +0530

    added the new logo to the website

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { useState, useEffect } from 'react';
-import { motion } from "framer-motion";
 import { playfair } from "@/components/layout/fonts";
+import { motion } from "framer-motion";
+import Image from 'next/image';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { Banner } from './Banner';
 
 export default function Header() {
@@ -17,7 +18,7 @@ export default function Header() {
 
     handleResize(); // Initial check
     window.addEventListener('resize', handleResize);
-    
+
     const handleScroll = () => {
       const scrollY = window.pageYOffset || document.documentElement.scrollTop;
       setShowBanner(scrollY < 100);
@@ -34,17 +35,18 @@ export default function Header() {
     <>
       <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50">
         <Banner showBanner={showBanner} isMobile={isMobile} />
-        
+
         <Link className='cursor-' href="/">
           <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center cursor-wait">
-            <motion.h1 
+            <motion.h1
               initial={{ opacity: 0, x: -20 }}
               animate={{ opacity: 1, x: 0 }}
               className={`${playfair.className} text-2xl sm:text-3xl md:text-4xl font-bold cursor-wait`}
             >
-              <span className="bg-gradient-to-r from-red-600 to-red-900 bg-clip-text text-transparent">
+              {/* <span className="bg-gradient-to-r from-red-600 to-red-900 bg-clip-text text-transparent">
                 Zinovaa
-              </span>
+              </span> */}
+              <Image src='/z.png' alt="Zinovaa" width={200} height={100} />
             </motion.h1>
           </div>
         </Link>
